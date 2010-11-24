936bb72d039565dfdde1274975de2a0aba391376
badjer <brian.dejong@gmail.com>
2010-11-23 21:39:46 -0800

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject clojure-yahoo-finance "1.0.0-SNAPSHOT"
+(defproject org.clojars.badjer/clojure-yahoo-finance "1.0.0-SNAPSHOT"
   :description "Clojure client for Yahoo! Finance historic data"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.httpcomponents/httpclient "4.1-alpha1"]
-                 [joda-time "1.6"]
-                 ]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+                 [joda-time "1.6"]]
 )
