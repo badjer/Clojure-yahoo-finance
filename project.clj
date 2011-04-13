e5fb350b7c97e0edd4ee002e73586a30ad533708
Zak Wilson <zak.wilson@gmail.com>
2011-04-13 05:17:23 -0400

    Updated dependency versions

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.clojars.badjer/clojure-yahoo-finance "1.0.0-SNAPSHOT"
+(defproject org.clojars.zak/clojure-yahoo-finance "1.0.0-SNAPSHOT"
   :description "Clojure client for Yahoo! Finance historic data"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.apache.httpcomponents/httpclient "4.1-alpha1"]
-                 [joda-time "1.6"]]
+                 [org.apache.httpcomponents/httpclient "4.1.1"]
+                 [joda-time "1.6.2"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
 )
